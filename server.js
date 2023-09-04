c2633fa9be3d13bd3f79f26e80f689676a80d4f7
HadiScript <2019cs519@gmail.com>
2023-09-04 18:50:06 +0500

    first

---
+++ server.js
@@ -0,0 +1,17 @@
+//  Requirments
+
+
+// client -> ticket -> submit -> ((title, description, file upload, category (tech, sales, etc), P-level (P0  urgent, p1 medium, p2 low, p3 very low  ))) 
+//        -> submit
+//        -> ticket -> status -> solved || discussion
+
+
+// admin   -> all rights
+// manager -> to Yank the tickets
+
+
+// agent   -> 
+
+// register (client), login, current user, create acccount 
+ 
+
