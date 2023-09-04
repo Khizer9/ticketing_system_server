e2fbb385e59786354488d36320147e020d931305
HadiScript <2019cs519@gmail.com>
2023-09-04 18:52:55 +0500

    just for check

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 //  Requirments
 
-
-// client -> ticket -> submit -> ((title, description, file upload, category (tech, sales, etc), P-level (P0  urgent, p1 medium, p2 low, p3 very low  ))) 
+// client -> ticket -> submit -> ((title, description, file upload, category (tech, sales, etc), P-level (P0  urgent, p1 medium, p2 low, p3 very low  )))
 //        -> submit
 //        -> ticket -> status -> solved || discussion
-
 
 // admin   -> all rights
 // manager -> to Yank the tickets
 
+// agent   ->
 
-// agent   -> 
+// register (client), login, current user, create acccount
 
-// register (client), login, current user, create acccount 
- 
+// console.log("am here");
 
+console.log("check ffor commit");
